7dc43682399db3cd81aaecf8e9a07de17ffe8796
humaid1379-cell <humaid1379@users.noreply.github.com>
2026-05-05 14:40:59 -0400

    Lower Arabic calligraphy background opacity to 30% (opacity: 0.3)

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -23,6 +23,7 @@ export default function HeroSection() {
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
+          opacity: 0.3,
         }}
       />
 
